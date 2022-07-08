0c49681a6ca10f31c8ea182c278bcb805197f23a
Pirate Patriot <sleepyhollowinoz@gmail.com>
2022-07-08 14:40:11 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Namecheap DNS updater
+
+<p align="center">
+  <a href="#development">Development</a> •
+  <a href="#architecture--documentation">Documentation</a> •
+  <a href="#how-to-contribute">Contribute</a> •
+  <a href="#support--feedback">Support</a>
+</p>
+
+Python script to detect when when your server ip changes and automate updating namecheap A records.
+
+---
+
+## Development
+
+### Setup
+
+1. requires python3 and packages as per requirements
+2. uses python virtual Environment to isolate from other projects.
+3. requires namecheap dns configuration to set A records to ip.
+   3a. see namecheap instructions <a href="https://www.namecheap.com/support/knowledgebase/article.aspx/319/2237/how-can-i-set-up-an-a-address-record-for-my-domain/" target="_blank" rel="noopener">how to set up an A (address) record for my domain</a>
+
+   3b. refer this screenshot of namecheap configuration <img src="namecheap_dns_config_screenshot.png" alt="name cheap dns settings example" width="250" height="300">
+3. follow the instructions on <a href="https://www.namecheap.com/support/knowledgebase/article.aspx/29/11/how-to-dynamically-update-the-hosts-ip-with-an-http-request/" target="_blank" rel="noopener">namecheap</a>
+4. create csv file containing a row for each combination of subdomain domain prefix &  domain. <br>
+NB: the password required is the 'Dynamic DNS Password' is same for each domain, row required for each subdomain.
+
+### Build
+
+1. python3 -m venv namecheap_script_ip_update_env
+2. source namecheap_script_ip_update_env/bin/activate
+3. pip install -r requirements.txt
+
+### Run
+
+1. cd <installed diretory>
+2. source namecheap_script_ip_update_env/bin/activate
+3. Run code
+```
+python3 update_namecheap_ip.py
+```
+4. use the troubleshooting/text options within the script (see comments in code).
+5. configure as a service to run on your *nix machine. edit 'update_namecheap_ip.service'
+<a href="https://medium.com/codex/setup-a-python-script-as-a-service-through-systemctl-systemd-f0cc55a42267" target="_blank" rel="noopener">follow these excellent instructions</a>.<br>
+NB : don't forget to enable the service to start during boot and test thoroughly.
+
+
+## How to Contribute
+
+raise an issue in the git issues tab with detailed description of issue.
+add comments to existing issues.
+create pull requests.
+
+#### How to run tests
+See comments in code.
+
+## Support & Feedback
+
+<a href="https://buymeacoffee.com/J6AYa7D" target="_blank" rel="noopener">Buy me a coffee</a>
---
+++ update_namecheap_ip.py
@@ -0,0 +1,95 @@
+import requests
+import json
+from bs4 import BeautifulSoup as bs
+import re
+import time
+from datetime import datetime
+import pandas as pd
+import os.path
+
+#path_to_file = "/home/ubuntu/f_projs/"
+path_to_file = "/mnt/d/2020/coding/namecheap_script_ip_update/"
+output_file = "namecheap_updates.txt"
+url_update_base = "https://dynamicdns.park-your-domain.com/update?"
+no_change_sleep=30
+config_file = "config_urls.csv"
+df_config = pd.read_csv(config_file)
+#https://dynamicdns.park-your-domain.com/update?host=[host]&domain=[domain_name]&password=[ddns_password]&ip=[your_ip]
+
+#test if output file exists, if not - create.
+if not os.path.exists(path_to_file+output_file):
+    #print("outputfile does not exist, creating empty file.")
+    with open(path_to_file+output_file, 'w') as fp:
+        pass
+
+def get():
+    endpoint = 'https://ipinfo.io/json'
+    response = requests.get(endpoint, verify = True)
+    if response.status_code != 200:
+        return 'Status:', response.status_code, 'Problem with the request. Exiting.'
+        exit()
+    data = response.json()
+    return data['ip']
+    #return "1.1.111.1" #used this for testing
+
+#get my ip
+#old_public_ip = get()
+#use this to force update first time this is run.
+old_public_ip = "0.0.0.0"
+#print("old_public_ip:", old_public_ip)
+
+
+def update_ip(url):
+    #
+    r = requests.get(url)
+    #soup = bs(r.text, 'html.parser')
+    soup = bs(r.text, 'xml')
+    errs = soup.find_all('errcount')
+    if len(errs)>0:
+        error_count = soup.find_all('errcount')[0].get_text()
+    else:
+        error_count = 0
+    done_tags = soup.find_all('Done')
+    if len(done_tags)>0:
+        done = soup.find_all('Done')[0].get_text()
+    else:
+        done = ""
+    return done=="true" and error_count==0
+
+
+while True:
+    #get public ip
+    new_public_ip = get()
+    #print("new_public_ip:", new_public_ip)
+    #if new_public_ip == old_public_ip: do nothing, sleep.
+    if new_public_ip == old_public_ip:
+        #print("no change, sleep. "+ str(datetime.now()))
+        #print("no change in ip, sleeping...")
+        time.sleep(no_change_sleep)
+    else:
+        #print("public ip changed, update namecheap records.")
+        #new_public_ip != old_public_ip
+        #
+        #print("change in ip detected, updating namecheap.")
+        url_update_base = "https://dynamicdns.park-your-domain.com/update?"
+        df_update_results = pd.DataFrame()
+        for index, row in df_config.iterrows():
+            #print(index, row["domain_prefix"], row["domain"], row["password"])
+            url = url_update_base \
+                + "host="      + row["domain_prefix"] \
+                + "&domain="   + row["domain"] \
+                + "&password=" + row["password"] \
+                + "&ip="       + new_public_ip
+            #print(url)
+            update_result = update_ip(url)
+            row['update_result']=update_result
+            row['update_time'] = str(datetime.now())
+            row['old_public_ip']=old_public_ip
+            row['new_public_ip']=new_public_ip
+            #print("row",row)
+            df_update_results = pd.concat([df_update_results, pd.DataFrame([row])], ignore_index=True)
+        #
+        #print("df_update_results\n", df_update_results)
+        df_update_results.to_csv(path_to_file+output_file, mode='a', index=False, header=False)
+        #
+        old_public_ip = new_public_ip
